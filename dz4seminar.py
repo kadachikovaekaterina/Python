ab9318397e80e5d4c21f653a91468d87ef70c973
kadachikovaekaterina <102862004+kadachikovaekaterina@users.noreply.github.com>
2022-07-25 19:14:51 +0400

    Add files via upload

--- dz4seminar.py
+++ dz4seminar.py
@@ -51,3 +51,29 @@
 # Спартак:2 0 0 2 0
 # Зенит:2 1 0 1 3
 # Локомотив:2 2 0 0 6
+
+
+n = int(input('Количество игр: '))
+x_list = [input('команда1;гол1;команда2;гол2: ').split(';') for x in range(n)]
+vs = [(x[0], x[2]) for x in x_list]
+import itertools
+clubs = set(itertools.chain.from_iterable(vs))
+res = {club:[0, 0, 0, 0, 0] for club in clubs}
+for kom1, gol1, kom2, gol2 in x_list:
+    res[kom1][0] += 1
+    res[kom2][0] += 1
+    if int(gol1) > int(gol2):
+        res[kom1][1] += 1
+        res[kom1][4] += 3
+        res[kom2][3] += 1
+    elif int(gol1) < int(gol2):
+        res[kom2][1] += 1
+        res[kom2][4] += 3
+        res[kom1][3] += 1
+    elif int(gol1) == int(gol2):
+        res[kom1][2] += 1
+        res[kom1][4] += 1
+        res[kom2][2] += 1
+        res[kom2][4] += 1
+for club in clubs:
+    print('{}: {}'.format(club, ' '.join(map(str, res[club]))))
